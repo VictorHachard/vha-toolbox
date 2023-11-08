84e608b34f0aeed25c7fcdd73acf3a08e802e2cc
Hachard Victor <31635811+VictorHachard@users.noreply.github.com>
2023-11-08 16:40:12 +0100

    Add pretty for generate_table

--- tests/test_table_generator.py
+++ tests/test_table_generator.py
@@ -42,6 +42,43 @@ class TableGeneratorTestCase(unittest.TestCase):
         generated_html_table = generate_table(rows, header)
         self.assertEqual(generated_html_table, expected_html_table)
 
+    def test_generate_table_with_incomplete_rows_no_header(self):
+        rows = [['Alice', 28], ['Bob', 35, 'Canada']]
+        expected_html_table = '<table><tr><td>Alice</td><td>28</td></tr><tr><td>Bob</td><td>35</td><td>Canada</td></tr></table>'
+
+        generated_html_table = generate_table(rows)
+        self.assertEqual(generated_html_table, expected_html_table)
+
+    def test_generate_table_with_pretty(self):
+        rows = [['Alice', 28, 'USA'], ['Bob', 35, 'Canada']]
+        header = ['Name', 'Age', 'Country']
+        expected_html_table = '<table>\n    <tr>\n        <th>Name</th>\n        <th>Age</th>\n        <th>Country</th>\n    </tr>\n    <tr>\n        <td>Alice</td>\n        <td>28</td>\n        <td>USA</td>\n    </tr>\n    <tr>\n        <td>Bob</td>\n        <td>35</td>\n        <td>Canada</td>\n    </tr>\n</table>'
+
+        generated_html_table = generate_table(rows, header, pretty=True)
+        self.assertEqual(generated_html_table, expected_html_table)
+
+    def test_generate_table_with_pretty_no_header(self):
+        rows = [['Alice', 28, 'USA'], ['Bob', 35, 'Canada']]
+        expected_html_table = '<table>\n    <tr>\n        <td>Alice</td>\n        <td>28</td>\n        <td>USA</td>\n    </tr>\n    <tr>\n        <td>Bob</td>\n        <td>35</td>\n        <td>Canada</td>\n    </tr>\n</table>'
+
+        generated_html_table = generate_table(rows, pretty=True)
+        self.assertEqual(generated_html_table, expected_html_table)
+
+    def test_generate_table_with_pretty_no_rows(self):
+        rows = []
+        header = ['Name', 'Age', 'Country']
+        expected_html_table = '<table>\n    <tr>\n        <th>Name</th>\n        <th>Age</th>\n        <th>Country</th>\n    </tr>\n</table>'
+
+        generated_html_table = generate_table(rows, header, pretty=True)
+        self.assertEqual(generated_html_table, expected_html_table)
+
+    def test_generate_table_with_pretty_no_rows_no_header(self):
+        rows = []
+        expected_html_table = '<table>\n</table>'
+
+        generated_html_table = generate_table(rows, pretty=True)
+        self.assertEqual(generated_html_table, expected_html_table)
+
 
 if __name__ == '__main__':
     unittest.main()
--- vha_toolbox/table_generator.py
+++ vha_toolbox/table_generator.py
@@ -1,13 +1,18 @@
 from typing import List
 
 
-def generate_table(rows: List[List[str]], header: List[str] = None) -> str:
+def generate_table(
+        rows: List[List[str]],
+        header: List[str] = None,
+        pretty: bool = False
+) -> str:
     """
     Convert a list of rows to an HTML table with a header row.
 
     Args:
         rows (list of lists): The rows of data for the table.
         header (list): The header row for the table.
+        pretty (bool): Whether to add newlines and indentation to the HTML table. Defaults to False.
 
     Returns:
         str: The HTML representation of the table.
@@ -15,23 +20,40 @@ def generate_table(rows: List[List[str]], header: List[str] = None) -> str:
     Example:
         >>> generate_table([['a', 'b'], ['c', 'd']])
         '<table><tr><th>a</th><th>b</th></tr><tr><td>c</td><td>d</td></tr></table>'
+        >>> generate_table([['a', 'b'], ['c', 'd']], header=['x', 'y'])
+        '<table><tr><th>x</th><th>y</th></tr><tr><td>a</td><td>b</td></tr><tr><td>c</td><td>d</td></tr></table>'
     """
     if header is None:
         header = []
 
     table = '<table>'
 
-    if header:
-        table += '<tr>'
-        for column in header:
-            table += f'<th>{column}</th>'
-        table += '</tr>'
+    if pretty:
+        if header:
+            table += '\n    <tr>'
+            for column in header:
+                table += f'\n        <th>{column}</th>'
+            table += '\n    '
+            table += '</tr>'
 
-    for row in rows:
-        table += '<tr>'
-        for column in row:
-            table += f'<td>{column}</td>'
-        table += '</tr>'
+        for row in rows:
+            table += '\n    <tr>'
+            for column in row:
+                table += f'\n        <td>{column}</td>'
+            table += '\n    </tr>'
+        table += '\n'
+    else:
+        if header:
+            table += '<tr>'
+            for column in header:
+                table += f'<th>{column}</th>'
+            table += '</tr>'
+
+        for row in rows:
+            table += '<tr>'
+            for column in row:
+                table += f'<td>{column}</td>'
+            table += '</tr>'
 
     table += '</table>'
     return table
