53b73af59998ffa724f2150ce694cb2909764d64
VictorHachard <nicolashachard@gmail.com>
2023-11-08 22:54:41 +0100

    Add magnet_link

--- vha_toolbox/__init__.py
+++ vha_toolbox/__init__.py
@@ -3,5 +3,6 @@ from .date_utils import *
 from .excel_column_conversion import *
 from .format_size import *
 from .highlight_text import *
+from .magnet_link import *
 from .string_manipulation import *
 from .table_generator import *
